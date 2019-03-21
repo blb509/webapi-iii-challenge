0fe7b3678f5861a3f43daf66f1fa973859318af8
Brandon L. Borrero <borrero509@outlook.com>
2019-03-20 20:08:16 -0700

    to upper case middleware done

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ const server = express();
 
 server.use(express.json());
 
+function toUpper(req, res, next) {
+  if (req.method == "POST" || req.method == "PUT") {
+    req.body.name = req.body.name.toUpperCase();
+  }
+  next();
+}
+
 server.get("/", (req, res) => {
   res.send(`
     <p>hi</p>
   `);
 });
 
+server.use("/api/users", toUpper);
 server.use("/api", usersRouter, postsRouter);
 
 module.exports = server;
